ba01cdff2f514ff936bd2590c07ef3fdf1766837
Osher <osherhacohen@gmail.com>
2021-02-14 18:39:36 +0200

    Initial and verty unfinished prototypes for commands. We may choose to implement otherwise.

---
+++ IMAP.Shared/Interfaces/ICommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IMAP.Shared.Interfaces
+{
+    public interface ICommand
+    {
+        public string CommandContent { get; set; }
+        public bool Validated { get; set; }
+        public bool ValidateCommand();
+
+                  
+    }
+}
