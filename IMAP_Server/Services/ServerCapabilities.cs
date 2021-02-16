287589e30526c57df5f23fa09c4fa61f06e17184
Osher <osherhacohen@gmail.com>
2021-02-16 15:10:47 +0200

    .

---
+++ IMAP_Server/Services/ServerCapabilities.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMAP_Server.Services
+{
+    public static class ServerCapabilities
+    {
+        //Here is the list of what the server is able to do.
+        public static List<string> CapabilitiesAny { get; private set; } = new List<string>() 
+        {
+            "IMAP4rev1",
+            "Add more capabilities here" //What the string says
+        };
+
+        //**************We might add more types of capabilities here, we'll need to see how it's implemented.
+
+    }
+}
