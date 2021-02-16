59de535a0e68fe6f0120fbdb11157f7f37998f5d
Osher <osherhacohen@gmail.com>
2021-02-16 15:11:35 +0200

    1

--- IMAP_Server/Services/ServerCapabilities.cs
+++ IMAP_Server/Services/ServerCapabilities.cs
@@ -12,7 +12,7 @@ namespace IMAP_Server.Services
         public static List<string> CapabilitiesAny { get; private set; } = new List<string>() 
         {
             "IMAP4rev1",
-            "Add more capabilities here" //What the string says
+            "Add more capabilities here" //What the string says, see here for a list: https://k9mail.app/documentation/development/imapExtensions.html
         };
 
         //**************We might add more types of capabilities here, we'll need to see how it's implemented.
