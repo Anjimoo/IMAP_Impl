c8a7216410cbecb5b7f9287a48d40e9538ad5a8b
Osher <osherhacohen@gmail.com>
2021-02-20 23:34:50 +0200

    Users.json should now be in debug.

--- IMAP_Server/Services/JsonParser.cs
+++ IMAP_Server/Services/JsonParser.cs
@@ -11,16 +11,17 @@ namespace IMAP_Server.Services
 {
     class JsonParser
     {
-        private static string AppPath => Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]);
-        private static DirectoryInfo myDir = new DirectoryInfo(AppPath);
-        public static string dataDir = myDir.Parent.FullName.ToString();
+        //private static string AppPath => Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]);
+        //private static DirectoryInfo myDir = new DirectoryInfo(AppPath);
+        //public static string dataDir = myDir.Parent.FullName.ToString();
 
 
         public static List<User> ReadUsers()
         {
-            string path = Path.Combine(dataDir,"Users.json"); 
+            //string path = Path.Combine(dataDir,"Users.json"); 
             List<User> users = new List<User>();
-            string jsonString = File.ReadAllText(path);
+            string jsonString = File.ReadAllText("Users.json");
+            //string jsonString = File.ReadAllText(path);
             users =JsonSerializer.Deserialize<List<User>>(jsonString);
             return users;
 
