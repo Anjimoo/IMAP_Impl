9711b51cdf7889568584619efb18ff7e6b092eef
Anjimoo <railanmoon@gmail.com>
2021-03-06 21:11:11 +0200

    added comments to select state commands

--- IMAP_Server/Services/CommandServices/SelectStateCommands.cs
+++ IMAP_Server/Services/CommandServices/SelectStateCommands.cs
@@ -20,13 +20,13 @@ namespace IMAP_Server.CommandModels
         private const int COPY_SPLIT = 4;
         private const int EXPUNGE_SPLIT = 2;
         private const int FETCH_SPLIT = 4;
-       
+
+        /* UNFINISHED */
         public static void Check(string[] command, Connection connectionState)
         {
             if (connectionState.SelectedState && command.Length == CHECK_SPLIT)
             {
-                /* UNFINISHED */
-                
+                  
             }
             else
             {
@@ -34,6 +34,22 @@ namespace IMAP_Server.CommandModels
             }
 
         }
+        /*
+         * The CLOSE command permanently removes all messages that have the
+         * \Deleted flag set from the currently selected mailbox, and returns
+         * to the authenticated state from the selected state. No untagged
+         * EXPUNGE responses are sent.
+         * No messages are removed, and no error is given, if the mailbox is
+         * selected by an EXAMINE command or is otherwise selected read-only.
+         * Even if a mailbox is selected, a SELECT, EXAMINE, or LOGOUT
+         * command MAY be issued without previously issuing a CLOSE command.
+         * The SELECT, EXAMINE, and LOGOUT commands implicitly close the
+         * currently selected mailbox without doing an expunge. However,
+         * when many messages are deleted, a CLOSE-LOGOUT or CLOSE-SELECT
+         * sequence is considerably faster than an EXPUNGE-LOGOUT or
+         * EXPUNGE-SELECT because no untagged EXPUNGE responses (which the
+         * client would probably ignore) are sent.
+         */
         public static void Close(string[] command, Connection connectionState)
         {
             if (connectionState.SelectedState && command.Length == CLOSE_SPLIT)
@@ -78,6 +94,21 @@ namespace IMAP_Server.CommandModels
                 connectionState.SendToStream($"{command[0]} BAD - command unknown or arguments invalid");
             }
         }
+        /*
+        The COPY command copies the specified message(s) to the end of the
+        specified destination mailbox. The flags and internal date of the
+        message(s) SHOULD be preserved, and the Recent flag SHOULD be set,
+        in the copy.
+        If the destination mailbox does not exist, a server SHOULD return
+        an error. It SHOULD NOT automatically create the mailbox. Unless
+        it is certain that the destination mailbox can not be created, the
+        server MUST send the response code "[TRYCREATE]" as the prefix of
+        the text of the tagged NO response. This gives a hint to the
+        the CREATE is successful.
+        If the COPY command is unsuccessful for any reason, server
+        implementations MUST restore the destination mailbox to its state
+        before the COPY attempt.
+         */
         public static void Copy(string[] command, Connection connectionState)
         {
             if (connectionState.SelectedState && command.Length == COPY_SPLIT)
@@ -126,6 +157,12 @@ namespace IMAP_Server.CommandModels
             }
 
         }
+        /*
+         The EXPUNGE command permanently removes all messages that have the
+        \Deleted flag set from the currently selected mailbox. Before
+        returning an OK to the client, an untagged EXPUNGE response is
+        sent for each message that is removed.
+         */
         public static void Expunge(string[] command, Connection connectionState)
         {
             if (connectionState.SelectedState && command.Length == EXPUNGE_SPLIT)
@@ -171,6 +208,23 @@ namespace IMAP_Server.CommandModels
 
 
         }
+        /*
+         The FETCH command retrieves data associated with a message in the
+        mailbox. The data items to be fetched can be either a single atom
+        or a parenthesized list.
+        Most data items, identified in the formal syntax under the
+        msg-att-static rule, are static and MUST NOT change for any
+        particular message. Other data items, identified in the formal
+        syntax under the msg-att-dynamic rule, MAY change, either as a
+        result of a STORE command or due to external events.
+        For example, if a client receives an ENVELOPE for a
+        message when it already knows the envelope, it can
+        safely ignore the newly transmitted envelope.
+        There are three macros which specify commonly-used sets of data
+        items, and can be used instead of data items. A macro must be
+        used by itself, and not in conjunction with other macros or data
+        items.
+         */
         public static void Fetch(string command, Connection connectionState)
         {
             var tag = command.Split(' ').First();
@@ -191,11 +245,13 @@ namespace IMAP_Server.CommandModels
                 connectionState.SendToStream($"{tag} BAD - command unknown or arguments invalid");
             }
         }
-        /// <summary>
-        /// Searches messages in mail box by specific criteria
-        /// </summary>
-        /// <param name="command"></param>
-        /// <param name="connectionState"></param>
+        /*
+         The SEARCH command searches the mailbox for messages that match
+        the given searching criteria. Searching criteria consist of one
+        or more search keys. The untagged SEARCH response from the server
+        contains a listing of message sequence numbers corresponding to
+        those messages that match the searching criteria.
+         */
         public static void Search(string[] command, Connection connectionState)
         {
             if (connectionState.SelectedState && command.Length > SEARCH_MINSPLIT)
@@ -224,19 +280,32 @@ namespace IMAP_Server.CommandModels
             }
 
         }
+        /*UNFINISHED*/
         public static void Store(string[] command, Connection connectionState)
         {
 
             if (connectionState.SelectedState && command.Length == STORE_SPLIT)
             {
-                // ******************************** TODO: UNFINISHED
-                //***************************************************
+                
             }
             else
             {
                 connectionState.SendToStream($"{command[0]} BAD - command unknown or arguments invalid");
             }
         }
+        /*
+         The UID command has two forms. In the first form, it takes as its
+        arguments a COPY, FETCH, or STORE command with arguments
+        appropriate for the associated command. However, the numbers in
+        the sequence set argument are unique identifiers instead of
+        message sequence numbers. Sequence set ranges are permitted, but
+        there is no guarantee that unique identifiers will be contiguous...
+
+        In the second form, the UID command takes a SEARCH command with
+        SEARCH command arguments. The interpretation of the arguments is
+        the same as with SEARCH; however, the numbers returned in a SEARCH
+        response for a UID SEARCH command are unique identifiers instead of message sequence numbers.
+         */
         public static void UID(string command, Connection connectionState)
         {
             var tag = command.Split(' ').First();
